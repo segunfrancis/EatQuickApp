ae107ca64ff7648603110e70405061904fee1c34
segunfrancis <francis4dblues@gmail.com>
2018-02-28 10:32:52 +0100

    Initial commit

---
+++ app/src/main/java/activity/PrivacyPolicyActivity.java
@@ -0,0 +1,15 @@
+package activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.tc1.eatquick2.R;
+
+public class PrivacyPolicyActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_privacy_policy);
+    }
+}
